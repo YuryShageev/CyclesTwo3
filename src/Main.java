9cc131642822a3a5f0b96acf2e86d41352728822
YuryShageev <schageev.yur@yandex.ru>
2022-10-09 19:39:44 +0300

    Третий блок второй части про циклы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int currentYear = 2022;
+        int asOfYear = currentYear - 200;
+        int ending = currentYear + 100;
+        for (int i = asOfYear; i < ending; ++i) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("2*" + i + " =" + 2 * i);
+        }
     }
 }
